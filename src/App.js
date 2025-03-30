025c3796e75c9e9bb0c964962cc5cccaff1f4734
BYTE90 <digarsingh2004@gmail.com>
2025-03-30 16:24:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./My_Components/Header";
+import {Footer} from "./My_Components/Footer";
+import  {Todos} from "./My_Components/Todos";
+import  {TodoItems} from "./My_Components/TodoItems";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title = "My Todos List"/>
+      <Todos/>
+      <Footer/>
+    </>
   );
 }
 
